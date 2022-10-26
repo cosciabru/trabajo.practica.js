ef763bdda26e924e61df418e77b9e5e2c0c43511
cosciabru <camilamc1806@icloud.com>
2022-10-26 03:00:17 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var nombre = "Camila";
+let apellido = "Coscia";
+const numero = "2996228141";
+console.log(nombre)
+console.log(apellido)
+console.log(numero)
+
+suma = (20 + 30)
+resta = (76 - 28)
+multiplicacion = (34 * 2)
+division = (45 / 4)
+console.log(suma)
+console.log(resta)
+console.log(multiplicacion)
+console.log(division)
+
+arreglo = [apellido = "Coscia", nombre = "Camila", apodo = "Bruna"]
+for (let value of arreglo) {
+    console.log(value)
+}
